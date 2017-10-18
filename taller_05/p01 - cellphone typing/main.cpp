03a30ceaeef966794e81a16c7b47c723ddc9d855
ignacio <hermosillaignacio@gmail.com>
2017-10-18 18:20:59 -0300

    taller 7

--- taller_05/p01 - cellphone typing/main.cpp
+++ taller_05/p01 - cellphone typing/main.cpp
@@ -1,22 +1,26 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1284
 // C++ implementation of search and insert
 // operations on Trie
 #include <bits/stdc++.h>
 
-const int MAX_NODES = 9 + 1; // TODO: update number for submision
+const int MAX_NODES = 1000000; // TODO: update number for submision
 int tree[MAX_NODES][26];
 int freq[MAX_NODES];
 int nodes;
 
-void init_trie() {
+void reset_trie()
+{
     memset(tree, -1, sizeof tree);
     memset(freq, 0, sizeof freq);
     nodes = 1;
 }
 
-void add_word(std::string & word) {
+void add_word(std::string & word)
+{
     int current = 0;
     freq[0]++;
-    for (char c : word) {
+    for (char c : word)
+    {
         int & nxt = tree[current][c - 'a'];
         if (nxt == -1) nxt = nodes++;
         current = nxt;
@@ -24,23 +28,52 @@ void add_word(std::string & word) {
     }
 }
 
-int freq_children(int father) {
-    // returns the frequency of the children
+int freq_node(int father)
+{
+    // returns the frequency of the current node
+    int n_father = father;
+    int f_father = freq[father];
+    int f_total = 0;
     bool leaf = true;
+
     for (int i = 0; i < 26; i++){
-        if (tree[father][i] == -1) leaf = false;
+        int n_child = tree[father][i];
+        if (n_child != -1)
+        {
+            if (f_father != freq[n_child] and leaf) f_total += f_father;
+            leaf = false;
+            f_total += freq_node(n_child);
+        }
     }
+    if (leaf) return 1;
+    return f_total;
 }
 
 
 int main()
 {
-    init_trie();
-    std::string s = std::string("hello");
-    add_word(s);
-    s = std::string("heaven");
-    add_word(s);
-    s = std::string("hell");
+    std::ios::sync_with_stdio(false); // makes cin/cout as fast as fscan and printf
+
+    std::cout << std::fixed;
+    std::cout << std::setprecision(2);
+
+    int n;
+
+    while(std::cin >> n) {
+        reset_trie();
+        float total = 0;
+        while(n--){
+            std::string s;
+            std::cin >> s;
+            add_word(s);
+        }
+        for (int i = 0; i < 26; i++){
+            if (tree[0][i] != -1) total += freq_node(tree[0][i]);
+        }
+        std::cout << total / float(freq[0]) << "\n";
+    }
+
+
 
 
 
