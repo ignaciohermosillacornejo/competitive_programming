25660fa7e132b0bb75d385953448d4daa1ef1988
ignaciohermosillacornejo <hermosillaignacio@gmail.com>
2017-12-05 13:01:26 -0300

    some work done, need more time for this

---
+++ taller_repechaje/p07.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+
+int main()
+{
+    int n;
+    int x;
+    std::cin >> n;
+    std::cin >> x;
+
+    n = n % 6;
+    while (n != 0){
+        if (n % 2 == 0){
+            if (x == 2) x = 1;
+            else if (x == 1) x = 2;
+        }
+        else {
+            if (x == 1) x = 0;
+            else if (x == 0) x = 1;
+        }
+        n--;
+    }
+
+
+    std::cout << x << "\n";
+}
---
+++ taller_repechaje/p08.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+std::queue < std::pair< int, int > > schedule;
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::pair <int, int> temp;
+    for (int i = 0; i < n; i++){
+        int a; int p;
+        std::cin >> a >> p;
+        temp = std::make_pair (a, p);
+        
+    }
+
+    return 0;
+}
