319ce663f7a4ac70d907459eb438cda030bafa35
ignaciohermosillacornejo <hermosillaignacio@gmail.com>
2019-05-24 16:14:40 -0400

    almost prime

---
+++ Code Forces/Almost Prime/Almost Prime.cpp
@@ -0,0 +1,108 @@
+/*
+Problem: https://codeforces.com/problemset/problem/26/A
+Tags: Math, Primes, Sieve Of Eratosthenes
+Status: 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+
+typedef vector<vi> graph;
+typedef vector<vii> wgraph;
+
+#ifndef declaraciones_h
+#define declaraciones_h
+
+#define rep(i, n) for (size_t i = 0; i < (size_t)n; i++)
+#define repx(i, a, b) for (int i = a; i < (int)b; i++)
+#define invrep(i, a, b) for (int i = b; i-- > (int)a;)
+
+#define pb push_back
+#define eb emplace_back
+#define ppb pop_back
+
+#define lg(x) (31 - __builtin_clz(x))
+#define lgg(x) (63 - __buitlin_clzll(x))
+#define gcd __gcd
+
+#define umap unordered_map
+#define uset unordered_set
+
+//ios::sync_with_stdio(0); cin.tie(0);
+//cout.setf(ios::fixed); cout.precision(4);
+
+#define debugx(x) cerr << #x << ": " << x << endl
+#define debugv(v)         \
+    cerr << #v << ":";    \
+    for (auto e : v)      \
+    {                     \
+        cerr << " " << e; \
+    }                     \
+    cerr << endl
+#define debugm(m)                                        \
+    cerr << #m << endl;                                  \
+    rep(i, (int)m.size())                                \
+    {                                                    \
+        cerr << i << ":";                                \
+        rep(j, (int)m[i].size()) cerr << " " << m[i][j]; \
+        cerr << endl;                                    \
+    }
+#define debugmp(m)                                                         \
+    cerr << #m << endl;                                                    \
+    rep(i, (int)m.size())                                                  \
+    {                                                                      \
+        cerr << i << ":";                                                  \
+        rep(j, (int)m[i].size())                                           \
+        {                                                                  \
+            cerr << " {" << m[i][j].first << "," << m[i][j].second << "}"; \
+        }                                                                  \
+        cerr << endl;                                                      \
+    }
+#define print(x) copy(x.begin(), x.end(), ostream_iterator<int>(cout, “”)), cout << endl
+
+#endif
+
+vi sieve(int n)
+{
+    vi primes;
+
+    vector<bool> is_prime(n + 1, true);
+    int limit = (int)floor(sqrt(n));
+    repx(i, 2, limit + 1) if (is_prime[i]) for (int j = i * i; j <= n; j += i)
+        is_prime[j] = false;
+
+    repx(i, 2, n + 1) if (is_prime[i]) primes.eb(i);
+
+    return primes;
+}
+
+int main()
+{
+    int n; 
+    cin >> n;
+
+    vi primes = sieve(3000);
+
+    int almost_primes = 0;
+    repx(i, 1, n + 1)
+    {
+        int count = 0;
+        for (auto prime: primes)
+        {
+            if (prime <= i and count != 3 and i % prime == 0)
+                count++;
+        }
+        if (count == 2)
+            almost_primes++;
+    }
+
+    cout << almost_primes << "\n";
+}
